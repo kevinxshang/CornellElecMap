30caa989673768862875e0fffd91db7d8202ea5c
Lillian Victoria Hwang-Geddes <lvh24@cornell.edu>
2024-04-02 11:56:31 -0400

    fixed web scraper bugs! yay

--- electricity_scraper.py
+++ electricity_scraper.py
@@ -1,4 +1,5 @@
 # reference: https://www.selenium.dev/documentation/webdriver/getting_started/first_script/ 
+# set to anaconda kernel (this does not work with all interpreters, not sure why)
 
 from selenium import webdriver
 #from selenium.webdriver.chrome.options import options
@@ -9,22 +10,37 @@ from selenium.webdriver.support.select import Select
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://portal.emcs.cornell.edu/dashboard/script/portal.js?orgId=2&var-portal_group=ToniMorrisonHall&kiosk=tv&from=1676523600000&to=1677646799000")
+
+
+driver.get("https://portal.emcs.cornell.edu/dashboard/script/portal.js?orgId=2&var-portal_group=ToniMorrisonHall")
+# ask chatgpt about conversion
+#driver.get("https://portal.emcs.cornell.edu/dashboard/script/portal.js?orgId=2&var-portal_group=ToniMorrisonHall")
 
 driver.implicitly_wait(60)
 
-# todo: fix this, find right label to use
-time_button = driver.find_element(by=By.XPATH, value='//button[@class="toolbar-button css-fbg4cg-toolbar-button"]')
+time_button = driver.find_element(by=By.XPATH, value='//button[@aria-label="Time range picker with current time range Last 24 hours selected"]')
+#time_button = driver.find_element(by=By.XPATH, value='//button[@testid="data-testid TimePicker Open Button"]')
 time_button.click()
+#time_button.click()
+
+# Note - need to fix this part, maybe wait a bit until downloading? apply button may not work
 
 start_time = driver.find_element(by = By.XPATH, value='//input[@aria-label="Time Range from field"]')
+start_time.clear()
 start_time.send_keys('2023-03-16 00:00:00')
 
+driver.implicitly_wait(60)
+
 end_time = driver.find_element(by = By.XPATH, value='//input[@aria-label="Time Range to field"]')
-start_time.send_keys('2023-03-31 23:59:59')
+end_time.clear()
+end_time.send_keys('2023-03-31 23:59:59')
+
+driver.implicitly_wait(60)
 
 apply_button = driver.find_element(by = By.XPATH, value='//button[@class="css-1sara2j-button"]')
-apply_button.click()
+apply_button.click() 
+
+driver.implicitly_wait(60)
 
 # todo: choose raw data
 data_type_button = driver.find_element(by = By.XPATH, value='//select[@name="slottype"]')
@@ -33,7 +49,7 @@ select_data.select_by_visible_text("No aggregation (raw data)")
 
 download_button = driver.find_element(by = By.XPATH, value='//button[@class="btn btn-success"]')
 download_button.click()
-#download_button.click()
+download_button.click()
 
 driver.implicitly_wait(60)
 
