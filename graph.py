15e021466bb7e86140c821ef45825c4eac227f26
Michael Chen <mzc8@cornell.edu>
2024-12-07 13:18:21 -0500

    bailey hall changing colors

--- graph.py
+++ graph.py
@@ -1,16 +1,21 @@
+import glob
 import pandas as pd
 import matplotlib.pyplot as plt
 
+folder_path = 'CornellElecMap/CornellElec_1.0/datasets/Time_15mins'
+files = glob.glob(f'{folder_path}/*.csv')
 
-data = pd.read_csv('CornellElec_1.0/datasets/Time_15mins/Cleaned_BaileyHall.csv')
-
-electricity_usage = data['value']
+values = []
+for file in files:
+    data = pd.read_csv(file)
+    electricity_usage = data[data['value'] < 1000]['value'].dropna()
+    values.extend(electricity_usage.tolist())
 
 plt.figure(figsize=(10, 6))
-plt.hist(electricity_usage, bins=30, color='blue', edgecolor='black', alpha=0.7)
+plt.hist(values, bins=30, color='blue', edgecolor='black', alpha=0.7)
 
 plt.title('Electricity Usage Distribution', fontsize=16)
-plt.xlabel('Electricity Usage (e.g., kWh)', fontsize=14)
+plt.xlabel('Electricity Usage (kWh)', fontsize=14)
 plt.ylabel('Frequency', fontsize=14)
 
 plt.show()
